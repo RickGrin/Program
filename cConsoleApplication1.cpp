622e955f053d955d39be6d304666c79c82be29ea
Voronov <repvbx1999@gmail.com>
2020-03-03 03:33:13 +0700

    labb1

---
+++ cConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int* p = new int[100];
+    int n = 0;
+
+ 
+    for (int i = 0; i < 10; i++) {
+        p[n] = rand() % 100;
+        cout << p[n] << ' ';
+        if (p[n] % 2)
+            n++;
+    }
+    cout << '\n';
+    for (int i = 0; i < n; i++)
+        cout << p[i] << ' ';
+    cout << '\n';
+}
